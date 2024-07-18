258710e207c44825c0ad6c8c61116ad8a92ef4a6
鄧莉瀕 <k18382003@gmail.com>
2024-07-18 14:48:36 -0700

    fixing Kinde LogoutLink import

--- src/app/create-candidate/page.jsx
+++ src/app/create-candidate/page.jsx
@@ -1,7 +1,5 @@
-import {
-  getKindeServerSession,
-  LogoutLink,
-} from '@kinde-oss/kinde-auth-nextjs/server';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components';
 import { redirect } from 'next/navigation';
 import CandidateCreateForm from '@/components/CandidateCreateForm';
 
@@ -20,7 +18,7 @@ export default async function Page() {
           type="button"
           className="rounded-md mr-5 bg-slate-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-2"
         >
-          <LogoutLink postLogoutRedirectURL="/">Log Out</LogoutLink>
+          <LogoutLink>Log Out</LogoutLink>
         </button>
       </div>
       <CandidateCreateForm />
